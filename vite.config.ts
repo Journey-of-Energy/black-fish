f9e1a488f9a272447744e0b8d9aa5a83f4c878e5
Tomáš Drga <tomas.drga1@gmail.com>
2024-07-16 15:25:53 +0200

    Added vite compression. Idk why my images are not loading on netlify, trying to find solution.

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import compression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    compression({
+      verbose: true,
+      disable: false,
+      deleteOriginFile: false,
+      threshold: 10240,
+      algorithm: "gzip",
+      ext: ".gz",
+    }),
+  ],
   server: {
     host: "0.0.0.0",
   },
